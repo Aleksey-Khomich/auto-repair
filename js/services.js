ea5eae819584bad82085fbe128443ad2085ba484
Irina Boiko <irinaboiko95@gmail.com>
2020-10-05 00:38:54 +0300

    fix prices, styles; add reviews, map

--- js/services.js
+++ js/services.js
@@ -8,9 +8,18 @@ tabsContainer.addEventListener('click', event => {
     
     if (targetClassList.contains('services_tab')) {
         for (let i = 0; i < contents.length; i++) {
+            let tabItem = tabs[i];
             let servicesItem = contents[i];
             
-            (servicesItem.dataset.content === target.dataset.tab) ? servicesItem.classList.add('opened') : servicesItem.classList.remove('opened');
+            //(servicesItem.dataset.content === target.dataset.tab) ? servicesItem.classList.add('opened') : servicesItem.classList.remove('opened');
+
+            if (servicesItem.dataset.content === target.dataset.tab) {
+                tabItem.classList.add('opened');
+                servicesItem.classList.add('opened');
+            } else {
+                tabItem.classList.remove('opened');
+                servicesItem.classList.remove('opened');
+            }
         }
     }
 });
